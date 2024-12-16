df7e8cf63731482cb52100dce280a6ff8fb0725f
Polaris <PolarisYan02@outlook.com>
2024-12-16 20:40:08 +0800

    feat: split file line count test

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ async function writeFile(filePath, content) {
   const baseName = path.basename(filePath, extname);
   await deleteSplitFiles(dir, baseName);
   const lines = content.split('\n');
-  const lineCountsPerFile = 170000;
+  const lineCountsPerFile = 180000;
   if (lines.length > lineCountsPerFile) {
     let newFilePath = dir;
     let counter = 0;
